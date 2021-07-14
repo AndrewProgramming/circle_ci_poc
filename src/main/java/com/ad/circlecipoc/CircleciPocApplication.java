ca5a830913dc39f82bde294e533945082a89e490
Andrew <kobe73er@gmail.com>
2021-07-14 10:32:22 +0800

    init

---
+++ src/main/java/com/ad/circlecipoc/CircleciPocApplication.java
@@ -0,0 +1,13 @@
+package com.ad.circlecipoc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CircleciPocApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(CircleciPocApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/ad/circlecipoc/CircleciPocApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ad.circlecipoc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CircleciPocApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
